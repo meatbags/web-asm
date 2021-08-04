b93a52ae61e6cd9cb742cc8f0e137d095de60900
Xavier Burrow <xavierb@temphost.local>
2021-08-04 16:55:15 +1000

    ?

--- src/app.js
+++ src/app.js
@@ -4,7 +4,29 @@ import WASMWorker from './wasm_worker';
 
 class App {
   constructor() {
+    // worker
     let w = new WASMWorker();
+
+    // WASM test
+    // WebAssembly.instantiateStreaming(fetch('./build/app.wasm'), {})
+		// .then(res => { console.log(res); });
+
+    let importObject = {
+      env: {
+        memoryBase: 0,
+        tableBase: 0,
+        memory: new WebAssembly.Memory({ initial: 256 }),
+        table: new WebAssembly.Table({ initial: 2, element: 'anyfunc' }),
+        abort: console.log,
+        env: {},
+      }
+    };
+    fetch('./build/app.wasm')
+      .then(res => res.arrayBuffer())
+      .then(bytes => WebAssembly.instantiate(bytes, importObject))
+      .then(res => {
+        console.log(res);
+      });
   }
 }
 
